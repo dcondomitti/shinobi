bdbf3458b6689584c9b83a412b8c8e0c4b840977
Moe <github@m03.ca>
2019-02-04 10:37:22 -0800

    Add Wasabi Hot Cloud Storage in cloudUploaders.js

--- libs/cloudUploaders.js
+++ libs/cloudUploaders.js
@@ -431,6 +431,130 @@ module.exports = function(s,config,lang){
             })
         }
     }
+    //Wasabi Hot Cloud Storage
+    var beforeAccountSaveForWasabiHotCloudStorage = function(d){
+        //d = save event
+        d.form.details.whcs_use_global=d.d.whcs_use_global
+        d.form.details.use_whcs=d.d.use_whcs
+    }
+    var cloudDiskUseStartupForWasabiHotCloudStorage = function(group,userDetails){
+        group.cloudDiskUse['whcs'].name = 'Wasabi Hot Cloud Storage'
+        group.cloudDiskUse['whcs'].sizeLimitCheck = (userDetails.use_whcs_size_limit === '1')
+        if(!userDetails.whcs_size_limit || userDetails.whcs_size_limit === ''){
+            group.cloudDiskUse['whcs'].sizeLimit = 10000
+        }else{
+            group.cloudDiskUse['whcs'].sizeLimit = parseFloat(userDetails.whcs_size_limit)
+        }
+    }
+    var loadWasabiHotCloudStorageForUser = function(e){
+        // e = user
+        var ar = JSON.parse(e.details)
+        if(ar.whcs_use_global === '1' && config.cloudUploaders && config.cloudUploaders.WasabiHotCloudStorage){
+            // {
+            //     whcs_accessKeyId: "",
+            //     whcs_secretAccessKey: "",
+            //     whcs_region: "",
+            //     whcs_bucket: "",
+            //     whcs_dir: "",
+            // }
+            ar = Object.assign(ar,config.cloudUploaders.WasabiHotCloudStorage)
+        }
+        //Wasabi Hot Cloud Storage
+        if(!s.group[e.ke].whcs &&
+           ar.whcs !== '0' &&
+           ar.whcs_accessKeyId !== ''&&
+           ar.whcs_secretAccessKey &&
+           ar.whcs_secretAccessKey !== ''&&
+           ar.whcs_region &&
+           ar.whcs_region !== ''&&
+           ar.whcs_bucket !== ''
+          ){
+            if(!ar.whcs_dir || ar.whcs_dir === '/'){
+                ar.whcs_dir = ''
+            }
+            if(ar.whcs_dir !== ''){
+                ar.whcs_dir = s.checkCorrectPathEnding(ar.whcs_dir)
+            }
+            var AWS = new require("aws-sdk")
+            s.group[e.ke].whcs = AWS
+            var wasabiEndpoint = new AWS.Endpoint('s3.wasabisys.com')
+            s.group[e.ke].whcs.config = new s.group[e.ke].whcs.Config({
+                endpoint: wasabiEndpoint,
+                accessKeyId: ar.whcs_accessKeyId,
+                secretAccessKey: ar.whcs_secretAccessKey,
+                region: ar.whcs_region
+            })
+            s.group[e.ke].whcs = new s.group[e.ke].whcs.S3();
+        }
+    }
+    var unloadWasabiHotCloudStorageForUser = function(user){
+        s.group[user.ke].whcs = null
+    }
+    var deleteVideoFromWasabiHotCloudStorage = function(e,video,callback){
+        // e = user
+        try{
+            var videoDetails = JSON.parse(video.details)
+        }catch(err){
+            var videoDetails = video.details
+        }
+        if(!videoDetails.location){
+            videoDetails.location = video.href.split('wasabisys.com')[1]
+        }
+        s.group[e.ke].whcs.deleteObject({
+            Bucket: s.group[e.ke].init.whcs_bucket,
+            Key: videoDetails.location,
+        }, function(err, data) {
+            if (err) console.log(err);
+            callback()
+        });
+    }
+    var uploadVideoToWasabiHotCloudStorage = function(e,k){
+        //e = video object
+        //k = temporary values
+        if(!k)k={};
+        //cloud saver - Wasabi Hot Cloud Storage
+        if(s.group[e.ke].whcs && s.group[e.ke].init.use_whcs !== '0' && s.group[e.ke].init.whcs_save === '1'){
+            var ext = k.filename.split('.')
+            ext = ext[ext.length - 1]
+            var fileStream = fs.createReadStream(k.dir+k.filename);
+            fileStream.on('error', function (err) {
+                console.error(err)
+            })
+            var saveLocation = s.group[e.ke].init.whcs_dir+e.ke+'/'+e.mid+'/'+k.filename
+            s.group[e.ke].whcs.upload({
+                Bucket: s.group[e.ke].init.whcs_bucket,
+                Key: saveLocation,
+                Body:fileStream,
+                ACL:'public-read',
+                ContentType:'video/'+ext
+            },function(err,data){
+                if(err){
+                    s.userLog(e,{type:lang['Wasabi Hot Cloud Storage Upload Error'],msg:err})
+                }
+                if(s.group[e.ke].init.whcs_log === '1' && data && data.Location){
+                    var save = [
+                        e.mid,
+                        e.ke,
+                        k.startTime,
+                        1,
+                        s.s({
+                            type : 'whcs',
+                            location : saveLocation
+                        }),
+                        k.filesize,
+                        k.endTime,
+                        data.Location
+                    ]
+                    s.sqlQuery('INSERT INTO `Cloud Videos` (mid,ke,time,status,details,size,end,href) VALUES (?,?,?,?,?,?,?,?)',save)
+                    s.setCloudDiskUsedForGroup(e,{
+                        amount : k.filesizeMB,
+                        storageType : 'whcs'
+                    })
+                    s.purgeCloudDiskForGroup(e,'whcs')
+                }
+            })
+        }
+    }
     //SFTP
     // var beforeAccountSaveForSftp = function(d){
     //     //d = save event
@@ -574,6 +698,15 @@ module.exports = function(s,config,lang){
     s.beforeAccountSave(beforeAccountSaveForBackblazeB2)
     s.onAccountSave(cloudDiskUseStartupForBackblazeB2)
     s.cloudDisksLoader('b2')
+    //wasabi
+    s.loadGroupAppExtender(loadWasabiHotCloudStorageForUser)
+    s.unloadGroupAppExtender(unloadWasabiHotCloudStorageForUser)
+    s.insertCompletedVideoExtender(uploadVideoToWasabiHotCloudStorage)
+    s.deleteVideoFromCloudExtensions['whcs'] = deleteVideoFromWasabiHotCloudStorage
+    s.cloudDiskUseStartupExtensions['whcs'] = cloudDiskUseStartupForWasabiHotCloudStorage
+    s.beforeAccountSave(beforeAccountSaveForWasabiHotCloudStorage)
+    s.onAccountSave(cloudDiskUseStartupForWasabiHotCloudStorage)
+    s.cloudDisksLoader('whcs')
     //SFTP
     // s.loadGroupAppExtender(loadSftpForUser)
     // s.unloadGroupAppExtender(unloadSftpForUser)
